3b92694193575ed8b46137ef21fe6e4d7005f29f
Abhiraamd5 <abhiraamd5@gmail.com>
2023-09-15 15:17:01 +0530

    first commit

---
+++ src/components/footer/Footer.jsx
@@ -0,0 +1,25 @@
+import React from 'react'
+import './footer.css'
+import {BsFacebook,BsTwitter,BsInstagram,BsGithub,BsLinkdin} from 'react-icons/bs'
+
+const Footer = () => {
+  return (
+    <footer>
+      <a href="#" className='footer__logo'>Abhiraam</a>
+      <div className="footer__socials">
+        <a href="https://www.facebook.com/Abhi Raam" target='_blank'><BsFacebook/></a>
+        <a href="https://www.instagram.com/abhiraamd/" target='_blank'><BsInstagram/></a>
+        
+        
+
+
+      </div>
+
+      <div className="footer__copyright">
+        <h4>&copy; Abhiraamd5. All rights reserved</h4>
+      </div>
+    </footer>
+  )
+}
+
+export default Footer
